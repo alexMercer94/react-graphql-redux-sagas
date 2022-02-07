617c6cb51a34e7e72eba4cd977a0470b1471ae97
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2022-02-06 19:20:34 -0600

    Added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
     return (
         <div>
+            <Header />
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/shop" element={<ShopPage />} />
