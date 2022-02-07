bc8fa8ab6d9afb8543683c1ba81390e2f6e65541
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2022-02-06 18:40:24 -0600

    Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+function HatsPage() {
+    return (
+        <div>
+            <h1>Hats page</h1>
+        </div>
+    );
+}
 
 function App() {
     return (
         <div>
-            <Homepage />
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+            </Routes>
         </div>
     );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ export default class Directory extends Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({ item, title, imageUrl, id, linkUrl, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );
