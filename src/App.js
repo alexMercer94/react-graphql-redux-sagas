2f332bb21eb19d57aa308baf96cf07b42276c7ab
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2022-02-06 19:10:12 -0600

    Addes shop page and components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-
-function HatsPage() {
-    return (
-        <div>
-            <h1>Hats page</h1>
-        </div>
-    );
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Homepage />} />
+                <Route path="/shop" element={<ShopPage />} />
             </Routes>
         </div>
     );
